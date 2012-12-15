719488f48076b7fb86050256ca925f2e32250b8a
mouseshi <linhongshimouse@gmail.com>
2012-12-15 10:55:44 +0800

    init project god~~

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pomodoro::Application.config.secret_token = 'aae92601125afa7025a2b05d721232d63499757862fd01d0e8ef4892ce4d0c58ce255f739ec52289c8208f3576920ca7a80524754b4589e01b4410e09f01cea7'
